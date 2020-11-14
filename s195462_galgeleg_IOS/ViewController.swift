b6d0288aaaf74f36e2f2d2718407aa81240b165b
kasasSulaiman <s195462@student.dtu.dk>
2020-11-14 22:13:14 +0100

    added welcome page

--- s195462_galgeleg_IOS/ViewController.swift
+++ s195462_galgeleg_IOS/ViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   // @IBOutlet var getStartedBt: UIButton!
+   
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+  /*
+        getStartedBt.backgroundColor = UIColor.blue
+        getStartedBt.layer.cornerRadius = getStartedBt.frame.height / 2
+        getStartedBt.setTitleColor(UIColor.white, for: .normal)
+        getStartedBt.layer.shadowColor = UIColor.red.cgColor
+        getStartedBt.layer.shadowRadius = 6
+ */
+        
+ 
+        
     }
+    
+  //  override func didReceiveMemoryWarning() {
+  //      super.didReceiveMemoryWarning()
+ //   }
 
 
 }
