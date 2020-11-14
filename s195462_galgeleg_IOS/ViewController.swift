0b1eafe261d7ef6c07ca1aed16015899daa91a14
kasasSulaiman <s195462@student.dtu.dk>
2020-11-14 21:17:31 +0100

    Initial Commit

---
+++ s195462_galgeleg_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  s195462_galgeleg_IOS
+//
+//  Created by Sulaiman Kasas on 14/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
